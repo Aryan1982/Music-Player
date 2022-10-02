3372f4acd0e694a878224500c8cf223801b436bd
Aryan1982 <ap1982o3@gmail.com>
2022-10-02 12:25:34 +0530

    done

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist')
 const music= document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress')
+const currentTimeEl = document.getElementById('current-time');
+const durationEl= document.getElementById('duration');
 const prevBtn= document.getElementById('prev');
 const playBtn= document.getElementById('play');
 const nextBtn= document.getElementById('next');
@@ -29,10 +33,13 @@ const songs=[
 		artist: 'Metric/Jacinto Design',
 	},
 ];
+
+
 //Check if Playing
 let isPlaying = false;
-//Play
 
+
+//Play
 function playSong(){
 	isPlaying=true;
 	playBtn.classList.replace('fa-play','fa-pause');
@@ -63,7 +70,80 @@ function loadSong(song){
 	image.src = `img/${song.name}.jpg`;
 
 }
+//Current Song
+let songIndex =0;
+
+
+//Next Song
+function nextSong(){
+	songIndex++;
+	if(songIndex > songs.length -1){
+		songIndex = 0;
+	}
+	console.log(songIndex)
+	loadSong(songs[songIndex]);
+	playSong()
+
+}
+
+//Prev Song
+function prevSong(){
+	songIndex--;
+	if(songIndex < 0){
+		songIndex = songs.length -1;
+	}
+	loadSong(songs[songIndex]);
+	playSong()
+
+}
+
 
 // On Load - select first song
+loadSong(songs[songIndex]);
 
-loadSong(songs[3]);
+// Update Progress Bar and Time
+function updateProgressBar(e){
+	if (isPlaying){
+		const {duration , currentTime}=e.srcElement;
+
+
+		//Update Progress Bar width
+		const progressPercent = (currentTime/duration)*100;
+		progress.style.width= `${progressPercent}%`
+
+
+		//Calculate display for duration
+		const durationMinutes = Math.floor(duration / 60);
+		let durationSeconds = Math.floor(duration % 60);
+		if (durationSeconds<10) {
+			durationSeconds = `0${durationSeconds}`
+		}
+		
+		//Delay switching duration Element to avoid NaN
+		if (durationSeconds){
+			durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+		}
+		//Calculate display for current
+		const currentMinutes = Math.floor(currentTime / 60);
+		let currentSeconds = Math.floor(currentTime % 60);
+		if (currentSeconds<10) {
+			currentSeconds = `0${currentSeconds}`
+		}
+		currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+	}
+}
+
+// Set Progress Bar
+function setProgressBar(e){
+	const width = this.clientWidth;
+	const clickX = e.offsetX;
+	const{ duration }= music;
+	music.currentTime= (clickX/width)*duration;
+}
+
+//Event Listeners
+prevBtn.addEventListener('click',prevSong);
+nextBtn.addEventListener('click',nextSong);
+music.addEventListener('ended', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
